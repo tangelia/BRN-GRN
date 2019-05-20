738b827bd09ba677fdb637caffd7fa7055de187f
tangelia <tangelia.atlanta@gmail.com>
2019-05-20 12:15:50 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT)
+})
