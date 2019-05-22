95b43b78789aafdfa3ee48f6fbfb197aeb481a7d
tangelia <tangelia.atlanta@gmail.com>
2019-05-22 13:22:03 -0400

    User routes

---
+++ routes.js
@@ -0,0 +1,63 @@
+//Controllers
+
+//Set admin
+const adminController = require('./controllers/admin_controller');
+//Set cloudinary
+const cloudinaryController = require('./controllers/cloudinary_controller');
+//Set user
+const userController = require('./controllers/user_controller');
+//Set products
+const productsController = require('./controllers/products_controller');
+
+
+const express = require('express');
+const router = express();
+
+
+
+setTimeout(() => {
+
+//   User Endpoint
+//   Cloudinary images upload
+  router.get('/api/upload', cloudinaryController.upload);
+  
+  //Read the user's session.
+  router.get('/api/user-data', userController.readUserData);
+  
+  //Add a item to cart
+  router.post('/api/user-data/cart', userController.addToCart);
+  
+  //Remove a item from the cart.
+  router.delete('/api/user-data/cart/:id',userController.removeFromCart);
+  
+  //User login
+  router.get('/auth/callback', userController.login);
+  // router.post('/api/login', userController.login)
+  
+  //User logout
+  router.post('/api/logout', userController.logout);
+  
+  
+  //Products Endpoints
+  //Get all the products
+  router.get('/api/products', productsController.readAllProducts);
+  //Get a specified product
+  
+  router.get('/api/products/:id', productsController.readProduct);
+  
+  
+//   Admin Endpoints
+  //Get admin users.
+  router.get('/api/users', adminController.getAdminUsers);
+  
+  //create product
+  router.post('/api/products', adminController.createProduct);
+  
+  //Update current product
+  router.put('/api/products/:id', adminController.updateProduct);
+  
+  //Delete product
+  router.delete('/api/products/:id', adminController.deleteProduct);
+  }, 200);
+
+  module.exports = router
--- server.js
+++ server.js
@@ -5,95 +5,24 @@ const bodyParser = require("body-parser");
 const session = require('express-session');
 const cors = require('cors');
 
-//Controllers
-
-//Set admin
-const adminController = require('./controllers/admin_controller');
-//Set cloudinary
-const cloudinaryController = require('./controllers/cloudinary_controller');
-//Set user
-const userController = require('./controllers/user_controller');
-//Set products
-const productsController = require('./controllers/products_controller');
-
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-
-//Connect to MongoDB
 
 
-// mongoose.connect(process.env.MONGODB_URI, (err) => {
-// if(err) {
-// console.log('Database Error', err);
-// }
-// console.log('Connected to database');
-// });
-
-//Middleware 
-app.use(bodyParser.json());
-    
-//Store cookies for user
-app.use(session({
-
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 14
-    }
-}));
-
-//Allow cross origin requests.
-app.use(cors());
-
-setTimeout(() => {
-
-// User Endpoint
-//Cloudinary images upload
-// app.get('/api/upload', cloudinaryController.upload);
-
-// //Read the user's session.
-// app.get('/api/user-data', userController.readUserData);
-
-// //Add a item to cart
-// app.post('/api/user-data/cart', userController.addToCart);
-
-// //Remove a item from the cart.
-// app.delete('/api/user-data/cart/:id',userController.removeFromCart);
-
-// //User login
-// app.get('/auth/callback', userController.login);
-// // app.post('/api/login', userController.login)
-
-// //User logout
-// app.post('/api/logout', userController.logout);
-
-
-//Products Endpoints
-//Get all the products
-app.get('/api/products', productsController.readAllProducts);
-//Get a specified product
-
-app.get('/api/products/:id', productsController.readProduct);
-
-
-//Admin Endpoints
-// //Get admin users.
-// app.get('/api/users', adminController.getAdminUsers);
-
-// //create product
-// app.post('/api/products', adminController.createProduct);
-
-// //Update current product
-// app.put('/api/products/:id', adminController.updateProduct);
-
-// //Delete product
-// app.delete('/api/products/:id', adminController.deleteProduct);
-}, 200);
-
-
-
+  const express = require('express')
+  const logger = require('morgan')
+  const app = express()
+  const routes = require('./routes/index.js')
+  
+  app.use(logger('dev'))
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static(`${__dirname}/brngrn/build`))
+  
+  app.use('/api/v1', routes)
+  
+  app.get('/*', (req, res) => {
+      res.sendFile(`${__dirname}/brngrn/build/index.html`)
+  })
+  
 
 const PORT = process.env.PORT || 3001
 
